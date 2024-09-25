9bafc8a6fb0a51492800314a2ad6b1bccb3aecdf
jeffersonrolino <jefferson.rolino@gmail.com>
2024-09-25 06:43:38 -0300

    test: create second test in TransactionServiceTest: createTransactionCase2

--- src/test/java/com/picpaydesafio/services/TransactionServiceTest.java
+++ src/test/java/com/picpaydesafio/services/TransactionServiceTest.java
@@ -4,6 +4,7 @@ import com.picpaydesafio.domain.user.User;
 import com.picpaydesafio.domain.user.UserType;
 import com.picpaydesafio.dtos.TransactionDTO;
 import com.picpaydesafio.repositories.TransactionRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -69,5 +70,28 @@ class TransactionServiceTest {
 
         verify(notificationService, times(1)).sendNotification(sender, "Transação realizada com sucesso");
         verify(notificationService, times(1)).sendNotification(receiver, "Transação realizada com sucesso");
+    }
+
+    @Test
+    @DisplayName("Should throw Exception when Transaction is not allowed")
+    void createTransactionCase2() throws Exception {
+        User sender = new User(1L, "Anthony", "Stark", "123456789",
+                "ironman@email.com","123", new BigDecimal(100), UserType.COMMON);
+
+        User receiver = new User(2L, "Steve", "Rogers", "987654321",
+                "captainamerica@email.com", "123", new BigDecimal(100), UserType.COMMON );
+
+
+        when(userService.findUserById(1L)).thenReturn(sender);
+        when(userService.findUserById(2L)).thenReturn(receiver);
+
+        when(authorizationService.authorizeTransaction()).thenReturn(false);
+
+        Exception thrown = Assertions.assertThrows(Exception.class, () -> {
+            TransactionDTO transactionDTO = new TransactionDTO(new BigDecimal(10), 1L, 2L);
+            transactionService.createTransaction(transactionDTO);
+        });
+
+        Assertions.assertEquals("Transação não autorizada", thrown.getMessage());
     }
 }
